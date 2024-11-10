5b5da5f091abb87a8ccd319be19e896e4083722c
ulian <ulian@JULY>
2024-11-10 17:35:55 +0200

    Добавьте файлы проекта.

---
+++ Labolatorna3task2/Labolatorna3task2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+   
+    double x;
+    cout << "Введіть значення x: ";
+    cin >> x;
+
+    
+    double numerator1 = sin(x) - pow(x, 2); 
+    double denominator1 = 2 * x + 1; 
+    double term1 = numerator1 / denominator1;
+
+    double numerator2 = pow(1 + x, x); 
+    double denominator2 = 1 + 3 * x; 
+    double term2 = numerator2 / denominator2;
+
+    double y = term1 + term2;
+
+   
+    cout << "Значення функції y: " << y << endl;
+
+    return 0;
+}
